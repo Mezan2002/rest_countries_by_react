5f4fad45412026fce0f69b75b4dc9975e917c8a2
Mezan2002 <mezanurr009@gmail.com>
2022-09-22 19:29:51 +0600

    Footer Added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright © All Rights is Reserved by Mezanur Rahman</p>
+    </footer>
+  );
+};
+
+export default Footer;
